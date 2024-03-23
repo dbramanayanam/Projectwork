439ff6b2e6b08334745aa84d5f2998f1f67ceff0
Dinesh babu <dbramanayanam@gmail.com>
2024-03-23 13:33:24 +0400

    modifying redis.sh

--- shell-script/redis.sh
+++ shell-script/redis.sh
@@ -35,20 +35,20 @@ permission
 dnf install https://rpms.remirepo.net/enterprise/remi-release-8.rpm -y &>> $LOGFILE
 validation $? "installing redis rpm file"
 
-dnf module enable redis:remi-6.2 -y
+dnf module enable redis:remi-6.2 -y &>> $LOGFILE
 validation $? "Enabling redis module"
 
-dnf install redis -y
+dnf install redis -y &>> $LOGFILE
 validation $? "installing redis"
 
-sed -i s/127.0.0.1/0.0.0.0/g /etc/redis.conf 
+sed -i s/127.0.0.1/0.0.0.0/g /etc/redis.conf &>> $LOGFILE 
 validation $? "Modifying redis is conf file /etc/redis.conf with 0.0.0.0"
 
-sed -i s/127.0.0.1/0.0.0.0/g /etc/redis/redis.conf
+sed -i s/127.0.0.1/0.0.0.0/g /etc/redis/redis.conf &>> $LOGFILE
 validation $? "Modifying /etc/redis/redis.conf with 0.0.0.0"
 
-systemctl enable redis
-validation $? "enable redis"
+systemctl enable redis  &>> $LOGFILE
+validation $? "enable redis". &>> $LOGFILE
 
-systemctl start redis
-validation $? "start redis"
+systemctl start redis &>> $LOGFILE
+validation $? "start redis". &>> $LOGFILE
