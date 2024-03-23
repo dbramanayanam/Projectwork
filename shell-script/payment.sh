08db515f088ab109135a1fb9f85da7e8e29ec887
Dinesh babu <dbramanayanam@gmail.com>
2024-03-23 15:25:53 +0400

    added payment.sh file

---
+++ shell-script/payment.sh
@@ -0,0 +1,18 @@
+component=payment
+source /home/centos/Projectwork/shell-script/common.sh
+
+permission
+dnf install python36 gcc python3-devel -y &>>$LOGFILE
+validation $? "Installing python"
+user_app_req
+
+cd /app &>>$LOGFILE
+pip3.6 install -r requirements.txt &>>$LOGFILE
+validation $? "Installing pyton dependencies"
+
+creating_service
+
+systemctl daemon-reload &>>$LOGFILE
+validation $? "reloading payment daemon"
+
+service_enable_start
